2da28e74e193bd2741def291ee1f0182e53bcdb6
J.R. Logan <admin@makehaven.org>
2022-09-11 17:34:48 -0400

    fix(__init__): listen on all interfaces

--- src/toolauth/__init__.py
+++ src/toolauth/__init__.py
@@ -1,6 +1,6 @@
 from quart import Quart, g
 from quart_schema import QuartSchema
-import sqlite3 #currently unused, but would like to...
+import sqlite3  # currently unused, but would like to...
 
 app = Quart(__name__)
 
@@ -8,5 +8,6 @@ QuartSchema(app)
 
 from toolauth import views
 
-def run(host='0.0.0.0') -> None:
-    app.run()
+
+def run(host="0.0.0.0") -> None:
+    app.run(host="0.0.0.0")
