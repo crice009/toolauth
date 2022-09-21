3a413ac5d3efdfb7ad6b0e9dfc6f5a4a82f7819c
Corey Rice <crice009@gmail.com>
2022-09-21 19:31:27 -0400

    added sqlite3 database functions

--- src/toolauth/__init__.py
+++ src/toolauth/__init__.py
@@ -1,12 +1,18 @@
 from quart import Quart, g
 from quart_schema import QuartSchema
-import sqlite3  # currently unused, but would like to...
+from pathlib import Path
+from sqlite3 import dbapi2 as sqlite3
+import os, sys
 
 app = Quart(__name__)
 
-QuartSchema(app)
+app.config.update(
+    {
+        "DATABASE": app.root_path / "toolauth.db",
+    }
+)
 
-from toolauth import views
+QuartSchema(app)
 
 
 def run() -> None:
@@ -17,3 +23,20 @@ def run() -> None:
     app.register_blueprint(crud_bp)
 
     app.run(host="0.0.0.0", port=8081)  # corey needs this in 8080 on WSL2
+
+
+def _connect_db():
+    engine = sqlite3.connect(app.config["DATABASE"])
+    engine.row_factory = sqlite3.Row
+    return engine
+
+
+def init_db():
+    db = _connect_db()
+    path = os.path.dirname(os.path.realpath(__file__))
+    path = os.path.join(path, "schema.sql")
+
+    with open(path, mode="r") as file_:
+        # print("madeit", file=sys.stdout)
+        db.cursor().executescript(file_.read())
+    db.commit()
